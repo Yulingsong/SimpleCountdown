6523a1c71ed6ac4247bca57a77ad8955a07e0323
Yulingsong <yulingsong5@126.com>
2016-01-18 17:07:26 +0800

    Initial Commit

---
+++ 1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1
+//
+//  Created by yulingsong on 16/1/18.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
